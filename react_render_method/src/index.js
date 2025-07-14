77953d8588043a4071a92bed93e6a590fc2302ba
jfeng3456j <jfeng3456j@gmail.com>
2025-07-13 22:52:02 -0700

    use root.render to element

--- react_render_method/src/index.js
+++ react_render_method/src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
+const element = (
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Phone</th>
+    </tr>
+
+    <tr>
+      <td>John Doe</td>
+      <td>314-900-9000</td>
+    </tr>
+  </table>
+);
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(<p>render react using react dom </p>);
+//root.render(<p>render react using react dom </p>);
+
+root.render(element);
