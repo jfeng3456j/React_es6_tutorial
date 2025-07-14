2ad2d97f206bd52c4ec4e46f0ea70ed9c83698e8
jfeng3456j <jfeng3456j@gmail.com>
2025-07-13 23:09:05 -0700

    learn to use react jsx

--- react_render_method/src/index.js
+++ react_render_method/src/index.js
@@ -1,21 +1,46 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-const element = (
-  <table>
-    <tr>
-      <th>Name</th>
-      <th>Phone</th>
-    </tr>
+//const element = <h1> Using JSX </h1>;
 
-    <tr>
-      <td>John Doe</td>
-      <td>314-900-9000</td>
-    </tr>
-  </table>
+//index
+const elementIndex = <h1> using index {30} </h1>
+
+
+//operator
+//const elementAdd = <h1> 3 + 3 is {3 + 3} </h1>
+//const elementSub = <h1> 3 - 3 is {3 - 3} </h1>
+//const elementMultiplication = <h1> 3 * 3 is {3 * 3} </h1>
+//const elementDivision = <h1> 3 / 3 is {3 / 3} </h1>
+
+//list
+/*
+const elementList = (
+    <ul>
+        <li>Apple</li>
+        <li>Orange</li>
+        <li>Peach</li>
+    </ul>
 );
+*/
+
+//condition
+const x = 10;
+let text = 'hello';
+
+if (x < 11) {
+    text = "What's up";
+}
+
+
+const elementCondition = <h1>conditional = {text} </h1>
+
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-//root.render(<p>render react using react dom </p>);
+//root.render(element);
 
-root.render(element);
+root.render(elementIndex);
+
+//root.render(elementAdd);
+
+//root.render(elementList);
