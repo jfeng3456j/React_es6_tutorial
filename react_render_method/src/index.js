c847b43ff3ef18ddbb065822013e7a2876c53f73
jfeng3456j <jfeng3456j@gmail.com>
2025-07-13 22:45:55 -0700

    use reactdom to render

---
+++ react_render_method/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<p>render react using react dom </p>);
